4408f15f738bf05405d72eb4aa19d1ba7de507d8
Don Wasinger <DonWasinger@Dons-MacBook-Pro.local>
2019-01-30 17:23:49 -0600

    layout

--- assets/app.js
+++ assets/app.js
@@ -1,11 +1,11 @@
 $(document).ready(function() {
     console.log("ready");
-    $("a").click(function() {
-        $("html, body").animate({
-            scrollTop: $( $(this).attr("href") ).offset().top 
-        }, 2 000);
-        return false;
-    });
-
 })
 
+$(function() {
+    $('a').click(function() {
+        $('html, body').animate({
+            scrollTop: $(this).attr("href").offset().top
+        }, 800);
+    });
+});
