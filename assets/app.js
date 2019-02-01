b0a7657fc06c2cd35396a77db5e00805195973a0
Don Wasinger <DonWasinger@Dons-MacBook-Pro.local>
2019-01-31 18:10:37 -0600

    resume

--- assets/app.js
+++ assets/app.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
     console.log("ready");
     $("a").click(function() {
         $("html, body").animate({
-            scrollTop: $( $(this).attr("href") ).offset().top 
+            scrollTop: $( $(this).attr("href") ).offset().top - 20 
         }, 1000);
         return false;
     });
